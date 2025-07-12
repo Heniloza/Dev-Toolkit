34b294b13d4d84875fc196455d82e5ada5d7fd95
Heniloza <ozahenil73@gmail.com>
2025-07-12 10:42:04 +0530

    Home layout problem solved

--- client/src/components/UI components/MainLayout.tsx
+++ client/src/components/UI components/MainLayout.tsx
@@ -17,7 +17,7 @@ function MainLayout() {
 
         {/* Main Content */}
         <div className="flex-1 overflow-hidden">
-          <main className="p-4 h-[calc(100vh-4rem)] overflow-y-auto">
+          <main className="p-4 mt-4 overflow-y-auto">
             <Outlet />
           </main>
         </div>
